eef0ea4c46aa7582dacc21ad9117460434f12295
wankhedeaastha <kishorwankhede66@gmail.com>
2024-07-13 00:36:17 +0530

    Add files via upload

---
+++ pizza_sales/16.sql
@@ -0,0 +1,16 @@
+-- Group the orders by data and calculate the average
+-- number of pizzas ordered per day.
+
+SELECT 
+    ROUND(AVG(quantity), 0) as avg_pizza_ordered_per_day
+FROM
+    (SELECT 
+        orders.order_date, SUM(order_details.quantity) AS quantity
+    FROM
+        orders
+    JOIN order_details ON orders.order_id = order_details.order_id
+    GROUP BY orders.order_date) AS order_quantity;
+    
+    
+    
+    
